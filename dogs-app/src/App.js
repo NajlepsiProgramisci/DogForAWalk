e0454d015405da6e04b5824230b53633327efa6a
MonikaMM77 <monikacodes@gmail.com>
2021-01-31 11:10:13 +0100

    login changes

--- dogs-app/src/App.js
+++ dogs-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Login from "../src/components/Login/Login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <h1>Wyprowadzanie psów</h1>
+          <Login />
       </div>
     );
   }
